95fc7b2e01d843c55d8969291d1d5294d0d22f01
Peter van Kranenburg <peter.van.kranenburg@meertens.knaw.nl>
2019-11-04 16:52:05 +0100

    Docstring for module

--- MTCFeatures/__init__.py
+++ MTCFeatures/__init__.py
@@ -1,1 +1,6 @@
+"""MTCFeatures provides melodies form the Meertens Tune Collections and ESSEN Folksong collection as sequences of features.
+
+The [Meertens Tune Collections](http://www.liederenbank.nl/mtc/) incorporate various data sets with melodic data. The melodies are provided in Humdrum **kern encoding and as MIDI sequences. In many cases, a representation of the melodies as sequences of feature values is needed. `MTCFeatures` is a Python module that provides such feature sequences together with functionality for feature and object filtering and feature extraction.
+"""
+
 name = "MTCFeatures"
